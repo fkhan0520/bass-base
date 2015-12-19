8cae6146bff670a02d7f8063128852d8dc7d4d1d
Dale Rivera <daleriverah@gmail.com>
2015-12-19 17:22:39 -0500

    Using scriptcam

--- js/app.js
+++ js/app.js
@@ -1,15 +1,5 @@
-var video = document.querySelector("#videoElement");
-
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
-
-if (navigator.getUserMedia) {       
-    navigator.getUserMedia({video: true}, handleVideo, videoError);
-}
-
-function handleVideo(stream) {
-    video.src = window.URL.createObjectURL(stream);
-}
-
-function videoError(e) {
-    // do something
-}
+$(document).ready(function() {
+    $('#my_camera').scriptcam({
+            path: '/js/'
+        });
+});
