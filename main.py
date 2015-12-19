ce1ba973399524651e1b8ffb8294a1aee28b0418
Dale Rivera <daleriverah@gmail.com>
2015-12-19 17:55:19 -0500

    Refactored a little

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ def get_emotion_values(image_url):
 	data = { "url": image_url}
 	headers = {"Content-Type": "application/json", "Ocp-Apim-Subscription-Key": "902567b4f75743ae8e039d37f9aeb972"}
 	r = requests.post(url, params = None, data = json.dumps(data), headers = headers)
-	print r.text
+	return r.text
 
-get_emotion_values("http://snappa.static.pressassociation.io/assets/2015/06/26090251/1435305770-36a7c3951a2bb484f033814ee652156a-600x398.jpg")
+print get_emotion_values("http://snappa.static.pressassociation.io/assets/2015/06/26090251/1435305770-36a7c3951a2bb484f033814ee652156a-600x398.jpg")
