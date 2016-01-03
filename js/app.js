d7c82fb2510701262786680fec7e309846aab0c1
Dale Rivera <dale.rivera@gatech.edu>
2016-01-03 16:31:56 -0500

    New try

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
-$(document).ready(function() {
-    $('#my_camera').scriptcam({
-            path: '/js/'
-        });
-});
+var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+var cameraStream;
+
+getUserMedia.call(navigator, {
+    video: true,
+    audio: false
+}, function (stream) {
+    /*
+    Here's where you handle the stream differently. Chrome needs to convert the stream
+    to an object URL, but Firefox's stream already is one.
+    */
+    if (window.webkitURL) {
+        video.src = window.webkitURL.createObjectURL(stream);
+    } else {
+        video.src = stream;
+    }
+
+    //save it for later
+    cameraStream = stream;
+
+    video.play();
+}, function () {});
