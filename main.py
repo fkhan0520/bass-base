d07a6a80b57400f4bddcc9ab3a84cabf8be4a87b
Jack Crawford <jackcrawfo@gmail.com>
2016-01-16 15:37:46 -0500

    emotionless boys

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def get_detected_face_id(image_url):
 	data = { "url": image_url}
 	headers = {"Content-Type": "application/json", "Ocp-Apim-Subscription-Key": "c1e6a2a46d7a4160829390d5de36b68c"}
 	r = requests.post(url, params = None, data = json.dumps(data), headers = headers)
-	return r.text
+	return r.json()[0]["faceId"]
 
 def is_face_match(faceId1, faceId2):
 	url = "https://api.projectoxford.ai/face/v1.0/verify" 
@@ -15,13 +15,4 @@ def is_face_match(faceId1, faceId2):
 	r = requests.post(url, params = None, data = json.dumps(data), headers = headers)
 	return r.text
 
-# returns face rectangle corners, dict of emotions to scores
-def get_emotion_values(image_url):
-	url = "https://api.projectoxford.ai/emotion/v1.0/recognize" 
-	data = { "url": image_url}
-	headers = {"Content-Type": "application/json", "Ocp-Apim-Subscription-Key": "cd990cb0d7e944c583a94a1b70ea3844"}
-	r = requests.post(url, params = None, data = json.dumps(data), headers = headers)
-	return r.text
-
-print get_emotion_values("http://snappa.static.pressassociation.io/assets/2015/06/26090251/1435305770-36a7c3951a2bb484f033814ee652156a-600x398.jpg")
 print get_detected_face_id("http://snappa.static.pressassociation.io/assets/2015/06/26090251/1435305770-36a7c3951a2bb484f033814ee652156a-600x398.jpg")
