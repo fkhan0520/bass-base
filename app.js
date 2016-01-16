a71b7c218269cc0660d177cc18bb6ca8db116c9e
Tony Shu <ti.ssue@gatech.edu>
2016-01-16 16:54:52 -0600

    firebase and camera

--- app.js
+++ app.js
@@ -10,18 +10,24 @@
 		}
 	}
 
-	function checkFace() {
-	  	$.ajax({
-		  type: "POST",
-		  url: "main.py",
-		  data: { param: text}
-		}).done(function( o ) {
-		   // do something
-		});
-	}
+	// function checkFace() {
+	//   	$.ajax({
+	// 	  type: "POST",
+	// 	  script: "main.py",
+	// 	  success: function(data,status){
+	// 	   // do something in js
+	// 	});
+	// }
 
 	exports.startApp = function() {
 		console.log('start app.');
+
+		var access_token = "c.oPExeHq4apmcPVgRHtPYXRLDwESaXw4OjLknx8mbYVBNVrd5mc1QXcZhDcQwMIDsnoZoBiE9nhU4Pquj5PGsyFIVN98IfiJM2ueqqgSTUTpXNvz5xipw3UbOpa4pzEuHKCY2R3ZYK5cbotad";
+		var ref = new Firebase('wss://developer-api.nest.com');
+		ref.auth(access_token);
+		ref.on('value', function(snapshot) {
+		console.log(snapshot.val());
+		});
 
 		playSong(true);
 	}
